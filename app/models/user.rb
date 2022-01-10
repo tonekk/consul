f65fc51fcc63c17b8bd1336c74d6506e002a046b
Finn Heemeyer <fh@tpwd.de>
2022-01-10 16:03:39 +0100

    Add login and registration through graphql

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  include GraphqlDevise::Concerns::Model
   include Verification
 
   devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable,
